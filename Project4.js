087e7de673ed01b1e3cb2fbd91cb4100b49e330d
Gweezdo <z.trivium@gmail.com>
2020-01-02 12:52:50 +1100

    Test Regex 4-6 works

--- Project4.js
+++ Project4.js
@@ -6,16 +6,27 @@ function telephoneCheck(str) {
     // let regex_2 = /^[(]\d{3}[)]\d{3}[-]\d{4}$/
     // console.log(regex_2.test(str));
 
-    let regex_3 = /^[(]\d{3}[)]\s\d{3}[-]\d{4}$/
-    console.log(regex_3.test(str));
+    // let regex_3 = /^[(]\d{3}[)]\s\d{3}[-]\d{4}$/
+    // console.log(regex_3.test(str));
+
+    // let regex_4 = /^\d{3}\s\d{3}\s\d{4}$/
+    // console.log(regex_4.test(str));
+
+    // let regex_5 = /^\d{10}$/
+    // console.log(regex_5.test(str));
+
+    let regex_6 = /^[1]\s\d{3}\s\d{3}\s\d{4}$/
+    console.log(regex_6.test(str));
+
+    
 
 
-    return true;
+    // return true;
 }
   
 // telephoneCheck("555-555-5555");
 // telephoneCheck("(555)555-5555");
-telephoneCheck("(555) 555-5555");
+// telephoneCheck("(555) 555-5555");
 // telephoneCheck("555 555 5555");
 // telephoneCheck("5555555555");
-// telephoneCheck("1 555 555 5555");
+telephoneCheck("1 555 555 5555");
