7469dd0eb4eb022a9bd799c16a60b31c6808e2be
Gweezdo <z.trivium@gmail.com>
2020-01-02 13:08:15 +1100

    First working solution - requires further iteration to include country code in front of all  strings 1-5

--- Project4.js
+++ Project4.js
@@ -1,32 +1,30 @@
 function telephoneCheck(str) {
     
-    // let regex_1 = /^\d{3}[-]\d{3}[-]\d{4}$/
-    // console.log(regex_1.test(str));
-
-    // let regex_2 = /^[(]\d{3}[)]\d{3}[-]\d{4}$/
-    // console.log(regex_2.test(str));
-
-    // let regex_3 = /^[(]\d{3}[)]\s\d{3}[-]\d{4}$/
-    // console.log(regex_3.test(str));
-
-    // let regex_4 = /^\d{3}\s\d{3}\s\d{4}$/
-    // console.log(regex_4.test(str));
-
-    // let regex_5 = /^\d{10}$/
-    // console.log(regex_5.test(str));
-
+    let regex_1 = /^\d{3}[-]\d{3}[-]\d{4}$/
+    let regex_2 = /^[(]\d{3}[)]\d{3}[-]\d{4}$/
+    let regex_3 = /^[(]\d{3}[)]\s\d{3}[-]\d{4}$/
+    let regex_4 = /^\d{3}\s\d{3}\s\d{4}$/
+    let regex_5 = /^\d{10}$/
     let regex_6 = /^[1]\s\d{3}\s\d{3}\s\d{4}$/
-    console.log(regex_6.test(str));
+
+    let test_1 = regex_1.test(str);
+    let test_2 = regex_2.test(str);
+    let test_3 = regex_3.test(str);
+    let test_4 = regex_4.test(str);
+    let test_5 = regex_5.test(str);
+    let test_6 = regex_6.test(str);
 
     
-
-
-    // return true;
+    if(test_1 || test_2 || test_3 || test_4 || test_5 || test_6){
+        return true;
+    }else{
+        return false;
+    }
 }
   
-// telephoneCheck("555-555-5555");
-// telephoneCheck("(555)555-5555");
-// telephoneCheck("(555) 555-5555");
-// telephoneCheck("555 555 5555");
-// telephoneCheck("5555555555");
+telephoneCheck("555-555-5555");
+telephoneCheck("(555)555-5555");
+telephoneCheck("(555) 555-5555");
+telephoneCheck("555 555 5555");
+telephoneCheck("5555555555");
 telephoneCheck("1 555 555 5555");
